16b4af4c4fbe61c327e84c92f1f9f2bf81552f1b
1lanphier <160071221+1lanphier@users.noreply.github.com>
2024-07-15 12:51:09 -0400

    Update setup.sh
    corrected wrong path bug.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+mkdir /var/www/html/htmltransfer/uploads;
 chmod +777 /var/www/html/htmltransfer/uploads;
-chmod +777 /var/www/html/htmltransfer/images;
-chmod +777 /var/www/html/htmltransfer/images/tenaxglitch.png;
+chmod +777 /var/www/html/htmltransfer/tenaxglitch.png;
